1404a0016ef479688996e89625057dbfb8d088b6
Thomas Beirne <thomas@buildingconnected.com>
2018-12-02 19:45:06 -0800

    Day 2, part 2

--- day2/main.clj
+++ day2/main.clj
@@ -19,10 +19,31 @@
                (get-count-map word)))
        0)))
 
-(defn part1 [input]
-  (let [lines (str/split-lines input)]
-    (*
-     (count (filter (has-n-of-char 2) lines))
-     (count (filter (has-n-of-char 3) lines)))))
+(defn part1 [lines]
+  (*
+   (count (filter (has-n-of-char 2) lines))
+   (count (filter (has-n-of-char 3) lines))))
 
-(println (part1 (slurp "input.txt")))
+(defn get-common-letters
+  [init-a init-b]
+  (loop [a init-a
+         b init-b
+         common-letters ""]
+    (cond
+      (or (empty? a) (empty? b)) common-letters
+      (= (first a) (first b)) (recur (subs a 1) (subs b 1) (str common-letters (first a)))
+      :else (recur (subs a 1)  (subs b 1) common-letters))))
+
+(defn part2 [ids]
+  (let [[a b]
+        (first (for [a ids
+                     b ids
+                     :while (not= a b)
+                     :when (= (- (count a) 1) (count (get-common-letters a b)))]
+                 (list a b)))]
+    (get-common-letters a b)))
+
+(defn get-lines [] (str/split-lines (slurp "./input.txt")))
+
+(println (part1 (get-lines)))
+(println (part2 (get-lines)))
